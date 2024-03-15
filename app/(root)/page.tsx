53696aaf1eed6ebaf612aaab017d1c07a47bbde3
Yash Vaidya <bobbynaldocr7@gmail.com>
2024-03-16 01:28:14 +0530

    Layout Sidebar & Mobile Navigation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,10 @@
-import { UserButton } from '@clerk/nextjs'
-import React from 'react'
-
 const Home = () => {
   return (
     <div>
       <p>Home</p>
 
-      <UserButton afterSignOutUrl='/' />
     </div>
   )
 }
 
-export default Home
+export default Home 
